9861372d141122b4424ea36d8a279a7acb2359c5
nixer89 <pbnixer@gmail.com>
2019-11-27 11:15:07 +0100

    v.3.2.0: changes for accessibility (benchmark by google lighthouse) + styling changes

--- src/app/components/footer.ts
+++ src/app/components/footer.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 export class AppFooterComponent {
     constructor() {
         var xrpTipbotScript = document.createElement("script");
-        xrpTipbotScript.setAttribute("id", "xrpTipbotScript");
+        xrpTipbotScript.setAttribute("id", "xrpTipbotScriptFooter");
         xrpTipbotScript.setAttribute("src", "https://www.xrptipbot.com/static/donate/tipper.js");
+        xrpTipbotScript.setAttribute("title", "XRPTipBot Button");
         document.body.appendChild(xrpTipbotScript);
     }
 }
--- src/app/components/topbar.ts
+++ src/app/components/topbar.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 export class AppTopbarComponent {
     constructor() {
         var xrpTipbotScript = document.createElement("script");
-        xrpTipbotScript.setAttribute("id", "xrpTipbotScript");
+        xrpTipbotScript.setAttribute("id", "xrpTipbotScriptTopbar");
         xrpTipbotScript.setAttribute("src", "https://www.xrptipbot.com/static/donate/tipper.js");
+        xrpTipbotScript.setAttribute("title", "XRPTipBot Button");
         document.body.appendChild(xrpTipbotScript);
     }
 }
